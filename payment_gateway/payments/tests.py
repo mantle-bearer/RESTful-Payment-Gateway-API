d70606df13bb827c8f9c34a8773cbcf718924ec3
Goodluck Chigozie <igbokwegoodluck8@gmail.com>
2025-01-21 15:32:43 -0800

    Made some file changes

--- payment_gateway/payments/tests.py
+++ payment_gateway/payments/tests.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
+
+# paymentTest class
 class PaymentTests(TestCase):
     def setUp(self):
         self.client = APIClient()
@@ -14,11 +16,12 @@ class PaymentTests(TestCase):
             'amount': 5000.00
         }
         response = self.client.post(reverse('payment-initiate'), data, format='json')
-        print(response.content)  # Add this line to see the error details
+        print(response.content)  # print the error details if there's any
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertIn('reference', response.data)
 
     def test_payment_status(self):
+
         # First, initiate a payment to ensure there is a valid reference
         data = {
             'customer_name': 'John Doe',
